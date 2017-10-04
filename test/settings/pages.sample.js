89e1d08e9c6ae72386b6b0ca6d303c442962746a
yagawa <yagawa@qzc.co.jp>
2017-10-04 17:13:12 +0900

    Change filename & directory

---
+++ test/settings/pages.sample.js
@@ -0,0 +1,17 @@
+const pages = [
+  {
+    name: 'top',
+    url: 'index.html'
+  },
+  {
+    name: 'sitemap',
+    url: 'sitemap.html'
+  },
+  {
+    name: 'inquiry',
+    url: 'inquiry.html'
+  }
+
+];
+
+module.exports = pages;
---
+++ test/settings/viewports.sample.js
@@ -0,0 +1,25 @@
+const viewports = [
+  {
+    name: 'H1920_1080',
+    width: 1920,
+    height: 1080
+  },
+  {
+    name: 'V1080_1920',
+    width: 1080,
+    height: 1920
+  },
+  {
+    name: 'H1080_600',
+    width: 1080,
+    height: 600
+  },
+  {
+    name: 'iPhone5_320_568',
+    width: 320,
+    height: 568
+  }
+
+];
+
+module.exports = viewports;
